6e3475abfef02de9259c105e2616c7952da2b91f
Beth <Beth@10.18.54.57>
2025-04-03 22:51:09 -0700

    initial upload

---
+++ Jsimscore/src/Domain/college.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Domain;
+
+/**
+ *
+ * @author Beth
+ */
+public class college {
+    int id;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    
+}
---
+++ Jsimscore/src/service/Idepartmentservice.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package service;
+
+/**
+ *
+ * @author Beth
+ */
+public interface Idepartmentservice {
+    boolean register(department department);
+    
+}
